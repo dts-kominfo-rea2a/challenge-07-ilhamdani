84542294d3e8c722849232c4cf2caf7b8f80ac6f
tanidev-tech <ilhamdani@gmail.com>
2022-06-11 21:56:40 -0500

    Update promise.js
    add function promiseOutput

--- promise.js
+++ promise.js
@@ -1,7 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  let theaterIXX = await promiseTheaterIXX()
+  let theaterVGC = await promiseTheaterVGC()
+  let total = 0
+
+  let datas = [theaterIXX, theaterVGC]
+  datas.map(values => {
+    values.map(value => {
+      if(value.hasil === emotion) total +=1
+    })
+  })
+
+  return total
+};
 
 module.exports = {
   promiseOutput,
